edeae4a6b13cba168aba7f7861b0da3e14544c8c
Sasha <31701038+SashaZHS@users.noreply.github.com>
2019-01-18 06:55:26 +0200

    Add files via upload

---
+++ Examination.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabCs
+{
+    class Examination
+    {
+        public int SemesterNum { get; private set; }
+        public string Subject { get; private set; }
+        public string Teacher { get; private set; }
+        public int Mark { get; private set; }
+        public bool IsDiff { get; private set; }
+        public DateTime ExaminationDate { get; private set; }
+
+        public Examination()
+        {
+            SemesterNum = 1;
+            Subject = "Object oriented programming";
+            Teacher = "Zhuk O. K.";
+            Mark = 60;
+            IsDiff = true;
+            ExaminationDate = new DateTime(2018, 1, 18);
+        }
+
+        public Examination(int NewNumber, string NewSubj, string NewTeacher, int NewMark, bool NewIsDiff, DateTime newDate)
+        {
+            SemesterNum = NewNumber;
+            Subject = NewSubj;
+            Teacher = NewTeacher;
+            Mark = NewMark;
+            IsDiff = NewIsDiff;
+            ExaminationDate = newDate;
+        }
+
+        public override string ToString()
+        {
+            return "Name of subject: " + Subject + "; Teacher: " +Teacher + "; Mark: " + Mark;
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabCs
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int BirthYear { 
+            get
+            {
+                return BirthDate.Year;
+            }
+            set
+            {
+                BirthDate = new DateTime(value, BirthDate.Month, BirthDate.Day);
+            }
+        }
+
+        public Person()
+        {
+            Name = "John";
+            SecondName = "Jons";
+            BirthDate = new DateTime(1980, 1, 1);
+        }
+
+        public Person(string NewName, string NewSecondName, DateTime NewBirthDate)
+        {
+            Name = NewName;
+            SecondName = NewSecondName;
+            BirthDate = NewBirthDate;
+        }
+
+        public virtual void PrintFullInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Second name: " + SecondName);
+            Console.WriteLine("Date of birth: " + BirthDate.ToShortDateString());
+        }
+
+        //task 2
+        public override bool Equals(object obj) {
+            Person temp = (Person)obj;
+            if(this.Name==temp.Name&& this.SecondName == temp.SecondName && this.BirthDate.Equals(temp.BirthDate))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
